a09319feaf0191e1cfac3e50868b989b3d512194
Vykintas <vykintas.rimeika@sa.stud.vu.lt>
2023-05-15 20:32:33 +0300

    testo logikos metodų išskyrimas

--- BezdzioneTests/ServerTests.cs
+++ BezdzioneTests/ServerTests.cs
@@ -1,5 +1,4 @@
-﻿using Bezdzione;
-using Bezdzione.Logs;
+﻿using Bezdzione.Logs;
 using Bezdzione.Request;
 using RestSharp;
 using System.Diagnostics;
@@ -18,13 +17,32 @@ namespace BezdzioneTests
         }
 
         [Test]
-        [TestCase(3)]
+        [TestCase(15)]
         public async Task TestServerDeployment(int timeout)
         {
             // Deploy given server
             int serverId = server.Deploy();
 
             string parameterInfo = $"region: {server.Parameters.RegionSlug}, plan: {server.Parameters.PlanSlug}, OS image: {server.Parameters.ImageSlug}";
+
+            HandleServerDeployment(serverId, parameterInfo);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            await WaitForServerActiveState(timeout);
+
+            stopwatch.Stop();
+            server.UpdateInfo();
+
+            AssertServerActive(stopwatch.Elapsed, timeout);
+
+            await Task.Delay(15000);
+            RestResponse response = server.Delete();
+            LogServerDeletionStatus(response.IsSuccessStatusCode);
+        }
+
+        private void HandleServerDeployment(int serverId, string parameterInfo)
+        {
             switch (serverId)
             {
                 case > 0:
@@ -34,11 +52,9 @@ namespace BezdzioneTests
                     Assert.Fail(MessageFormatter.Error($"Failed to deploy server with {parameterInfo}."));
                     break;
             }
-
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            //Check server state
+        }
+        private async Task WaitForServerActiveState(int timeout)
+        {
             string serverState = server.GetState();
             DateTime startTime = DateTime.Now;
 
@@ -46,29 +62,33 @@ namespace BezdzioneTests
             {
                 if ((DateTime.Now - startTime) > TimeSpan.FromMinutes(timeout))
                 {
-                    // if the maximum wait format is exceeded, log an error and fail the test
-                    FileLogger.Log(MessageFormatter.Info($"Timeout - server took more than {timeout} mins to become active."));
-                    server.UpdateInfo();
-                    server.Delete();
-                    Assert.Fail($"Timeout - server took more than {timeout} mins to become active.");
+                    HandleServerTimeout(timeout);
                 }
 
-                await Task.Delay(5000); // Wait 5 seconds before checking server state again
+                await Task.Delay(5000);
                 serverState = server.GetState();
             }
+        }
 
-            stopwatch.Stop();
+        private void HandleServerTimeout(int timeout)
+        {
+            FileLogger.Log(MessageFormatter.Info($"Timeout - server took more than {timeout} mins to become active."));
             server.UpdateInfo();
+            server.Delete();
+            Assert.Fail($"Timeout - server took more than {timeout} mins to become active.");
+        }
 
-            //Assert that server became active within allowed time in minutes
-            ConsoleLogger.Log(MessageFormatter.Info($"Server took {stopwatch.Elapsed} to become active"));
-            FileLogger.Log(MessageFormatter.Info($"Server took {stopwatch.Elapsed} to become active"));
-            Assert.That(stopwatch.Elapsed, Is.LessThan(TimeSpan.FromMinutes(timeout)), $"Timeout. Server took {stopwatch.Elapsed} to become active. Maximum allowed time - {timeout} minutes.");
+        private void AssertServerActive(TimeSpan elapsed, int timeout)
+        {
+            ConsoleLogger.Log(MessageFormatter.Info($"Server took {elapsed} to become active"));
+            FileLogger.Log(MessageFormatter.Info($"Server took {elapsed} to become active"));
+            Assert.That(elapsed, Is.LessThan(TimeSpan.FromMinutes(timeout)), $"Timeout. Server took {elapsed} to become active. Maximum allowed time - {timeout} minutes.");
+        }
 
-            await Task.Delay(15000);
-            RestResponse response = server.Delete();
-            ConsoleLogger.Log(response.IsSuccessStatusCode ? MessageFormatter.Info("Server successfully deleted.") : MessageFormatter.Error("Failed to delete the server"));
-            FileLogger.Log(response.IsSuccessStatusCode ? MessageFormatter.Info("Server successfully deleted.") : MessageFormatter.Error("Failed to delete the server"));
+        private void LogServerDeletionStatus(bool isSuccess)
+        {
+            ConsoleLogger.Log(isSuccess ? MessageFormatter.Info("Server successfully deleted.") : MessageFormatter.Error("Failed to delete the server"));
+            FileLogger.Log(isSuccess ? MessageFormatter.Info("Server successfully deleted.") : MessageFormatter.Error("Failed to delete the server"));
         }
     }
 }
