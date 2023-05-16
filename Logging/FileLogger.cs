e7ef0865414de7b4f7c4574c81708f3aa1399f3c
Vykintas <vykintas.rimeika@sa.stud.vu.lt>
2023-05-17 02:05:21 +0300

    Papildytas FileLogger funkcionalumas sukuriant failą ir direktoriją, jeigu jų nėra

--- Logging/FileLogger.cs
+++ Logging/FileLogger.cs
@@ -2,12 +2,34 @@
 {
     public static class FileLogger
     {
+        private static readonly string LogDirectoryPath =
+            Path.Combine(Environment.CurrentDirectory, "Logs");
+
         private static readonly string LogFilePath =
-            Path.Combine(Environment.CurrentDirectory, "Logs", "logfile.txt");
+            Path.Combine(LogDirectoryPath, "logfile.txt");
 
         public static void Log(string message)
         {
+            EnsureLogDirectoryExists();
+            EnsureLogFileExists();
+
             File.AppendAllText(LogFilePath, message);
+        }
+
+        private static void EnsureLogDirectoryExists()
+        {
+            if (!Directory.Exists(LogDirectoryPath))
+            {
+                Directory.CreateDirectory(LogDirectoryPath);
+            }
+        }
+
+        private static void EnsureLogFileExists()
+        {
+            if (!File.Exists(LogFilePath))
+            {
+                File.Create(LogFilePath).Close();
+            }
         }
     }
 }
